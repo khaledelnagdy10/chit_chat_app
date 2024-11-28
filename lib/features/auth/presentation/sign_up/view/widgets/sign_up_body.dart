615040dbffac0d6d81aa707bc455fec53db82321
khaledelnagdy10 <128099437+khaledelnagdy10@users.noreply.github.com>
2024-11-28 17:58:29 +0200

    add GlobalKey

--- lib/features/auth/presentation/sign_up/view/widgets/sign_up_body.dart
+++ lib/features/auth/presentation/sign_up/view/widgets/sign_up_body.dart
@@ -5,6 +5,7 @@ import 'package:chit_chat/core/utils/logo.dart';
 import 'package:chit_chat/core/utils/text_styles.dart';
 import 'package:chit_chat/core/utils/widgets/custom_button.dart';
 import 'package:chit_chat/core/utils/widgets/custom_text_form_field.dart';
+import 'package:chit_chat/core/utils/widgets/scaffold_messanger.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -19,79 +20,92 @@ class SignUpBody extends StatefulWidget {
 class _SignUpBodyState extends State<SignUpBody> {
   String emailAddress = '';
   String password = '';
+
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Groupie',
-              style: Style.textStyleBold35,
-            ),
-          ],
-        ),
-        Text(
-          'Login now to see what they are talking!',
-          style: Style.textStyle18.copyWith(color: Colors.grey[700]),
-        ),
-        const Logo(),
-        CustomTextFormField(
-          lableText: 'Email',
-          prefixIcon: const Icon(Icons.email),
-          onChanged: (String value) {
-            setState(() {
-              emailAddress = value;
-              log(emailAddress);
-            });
-          },
-        ),
-        CustomTextFormField(
-          lableText: 'Password',
-          prefixIcon: const Icon(Icons.lock),
-          onChanged: (String value) {
-            setState(() {
-              password = value;
-              log(password);
-            });
-          },
-        ),
-        SizedBox(
-          width: 370,
-          child: CustomButton(
-            text: 'Sign up',
-            onPressed: () async {
-              try {
-                final credential =
-                    await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                  email: emailAddress,
-                  password: password,
-                );
-              } on FirebaseAuthException catch (e) {
-                if (e.code == 'weak-password') {
-                  print('The password provided is too weak.');
-                } else if (e.code == 'email-already-in-use') {
-                  print('The account already exists for that email.');
-                }
-              } catch (e) {
-                print(e);
-              }
+    return Form(
+      key: formKey,
+      child: Column(
+        children: [
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Groupie',
+                style: Style.textStyleBold35,
+              ),
+            ],
+          ),
+          Text(
+            'Login now to see what they are talking!',
+            style: Style.textStyle18.copyWith(color: Colors.grey[700]),
+          ),
+          const Logo(),
+          CustomTextFormField(
+            lableText: 'Email',
+            prefixIcon: const Icon(Icons.email),
+            onChanged: (String value) {
+              setState(() {
+                emailAddress = value;
+                log(emailAddress);
+              });
             },
           ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('I already have an account \t'),
-            GestureDetector(
-                child: Text(
-              'Log In',
-              style: Style.textStyle18.copyWith(color: Colors.amber[800]),
-            )),
-          ],
-        ),
-      ],
+          CustomTextFormField(
+            lableText: 'Password',
+            prefixIcon: const Icon(Icons.lock),
+            onChanged: (String value) {
+              setState(() {
+                password = value;
+                log(password);
+              });
+            },
+          ),
+          SizedBox(
+            width: 370,
+            child: CustomButton(
+              text: 'Sign up',
+              onPressed: () async {
+                if (formKey.currentState!.validate()) {
+                  try {
+                    UserCredential credential = await FirebaseAuth.instance
+                        .createUserWithEmailAndPassword(
+                      email: emailAddress,
+                      password: password,
+                    );
+                    customScaffoldMessanger(
+                        context, 'Sucssesfull', Colors.green);
+                  } on FirebaseAuthException catch (e) {
+                    if (e.code == 'weak-password') {
+                      customScaffoldMessanger(context,
+                          'The password provided is too weak', Colors.red);
+                    } else if (e.code == 'email-already-in-use') {
+                      customScaffoldMessanger(
+                          context,
+                          'The account already exists for that email',
+                          Colors.red);
+                    }
+                  } catch (e) {
+                    customScaffoldMessanger(context, e.toString(), Colors.red);
+                  }
+                }
+              },
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('I already have an account \t'),
+              GestureDetector(
+                  child: Text(
+                'Log In',
+                style: Style.textStyle18.copyWith(color: Colors.amber[800]),
+              )),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
