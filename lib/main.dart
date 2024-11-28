05d0abd35591f92f0318428554b7c6f9b0dd3dcf
khaledelnagdy10 <128099437+khaledelnagdy10@users.noreply.github.com>
2024-11-28 17:10:34 +0200

    Initialise Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:chit_chat/features/auth/presentation/sign_up/view/sign_up_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const ChitChat());
 }
 
