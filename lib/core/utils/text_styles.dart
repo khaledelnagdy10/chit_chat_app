b40d482814ce5ca99f425efd09b4f45b458d888d
khaledelnagdy10 <128099437+khaledelnagdy10@users.noreply.github.com>
2024-11-28 15:44:50 +0200

    add text style in core

---
+++ lib/core/utils/text_styles.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+abstract class Style {
+  static const textStyle18 = TextStyle(fontSize: 18, color: Colors.black);
+
+  static const textStyle24 = TextStyle(fontSize: 24, color: Colors.black);
+
+  static const textStyleBold24 =
+      TextStyle(fontSize: 24, color: Colors.black, fontWeight: FontWeight.bold);
+
+  static const textStyleBold35 =
+      TextStyle(fontSize: 35, color: Colors.black, fontWeight: FontWeight.bold);
+}
